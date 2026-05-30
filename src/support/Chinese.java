87f3f4b9db9f8a8c5033d5525950ce7b079dfc71
Earth-Instrument <12510222@mail.sustech.edu.cn>
2026-05-30 19:39:33 +0800

    为了运行，暂时未实现两个语言类

--- src/support/Chinese.java
+++ src/support/Chinese.java
@@ -3,6 +3,31 @@ package support;
 public enum Chinese implements Language{
     INSTANCE;
 
+    @Override
+    public String loginTitle() {
+        return "";
+    }
+
+    @Override
+    public String loginLogin() {
+        return "";
+    }
+
+    @Override
+    public String loginReg() {
+        return "";
+    }
+
+    @Override
+    public String loginExit() {
+        return "";
+    }
+
+    @Override
+    public String loginGuest() {
+        return "";
+    }
+
     @Override public String menuTitle() { return "夏日大挑战 - 主菜单"; }
     @Override public String menuWelcome(String username) { return "欢迎，" + username; }
     @Override public String menuStartGame() { return "开始游戏"; }
@@ -10,5 +35,11 @@ public enum Chinese implements Language{
     @Override public String menuExit() { return "退出游戏"; }
     @Override public String getLanguageTooltip() { return "切换语言"; }
     @Override public String getHelpTooltip() { return "帮助"; }
+
+    @Override
+    public String gameSave() {
+        return "";
+    }
+
 
 }
--- src/support/English.java
+++ src/support/English.java
@@ -3,6 +3,31 @@ package support;
 public enum English implements Language {
     INSTANCE;
 
+    @Override
+    public String loginTitle() {
+        return "";
+    }
+
+    @Override
+    public String loginLogin() {
+        return "";
+    }
+
+    @Override
+    public String loginReg() {
+        return "";
+    }
+
+    @Override
+    public String loginExit() {
+        return "";
+    }
+
+    @Override
+    public String loginGuest() {
+        return "";
+    }
+
     @Override public String menuTitle() { return "Summer Challenge - Main Menu"; }
     @Override public String menuWelcome(String username) { return "Welcome, " + username; }
     @Override public String menuStartGame() { return "Start Game"; }
@@ -10,4 +35,11 @@ public enum English implements Language {
     @Override public String menuExit() { return "Exit"; }
     @Override public String getLanguageTooltip() { return "Change Language"; }
     @Override public String getHelpTooltip() { return "Help"; }
+
+    @Override
+    public String gameSave() {
+        return "";
+    }
+
+
 }
